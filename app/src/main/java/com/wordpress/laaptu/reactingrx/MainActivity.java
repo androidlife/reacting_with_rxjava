bbad29fd220f37619dca001012d6d3aa7b41c4d5
laaptu <sanimap@gmail.com>
2017-01-19 13:00:46 +0545

    Working with schedulers in RxJava

--- app/src/main/java/com/wordpress/laaptu/reactingrx/MainActivity.java
+++ app/src/main/java/com/wordpress/laaptu/reactingrx/MainActivity.java
@@ -3,6 +3,7 @@ package com.wordpress.laaptu.reactingrx;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.wordpress.laaptu.reactingrx.basics.ProducerConsumerFragment;
+import com.wordpress.laaptu.reactingrx.basics.SubscribeObserveFragment;
 import com.wordpress.laaptu.reactingrx.basics.SubscriberOtherMethodsFragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     //addBasicFragment();
+    //subscriberOtherMethodTest();
     subscribeObserveTest();
   }
 
@@ -20,9 +22,15 @@ public class MainActivity extends AppCompatActivity {
         ProducerConsumerFragment.getInstance(null)).commit();
   }
 
-  private void subscribeObserveTest() {
+  private void subscriberOtherMethodTest() {
     getSupportFragmentManager().beginTransaction()
         .replace(R.id.container, SubscriberOtherMethodsFragment.getInstance(null))
+        .commit();
+  }
+
+  private void subscribeObserveTest() {
+    getSupportFragmentManager().beginTransaction()
+        .replace(R.id.container, SubscribeObserveFragment.getInstance(null))
         .commit();
   }
 }
